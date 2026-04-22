0957ad4e1bf80a7b2291b092c4c67e2fdb83abe6
ytruenightstory-png <ytruenightstory@gmail.com>
2026-04-22 08:26:38 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+    WORKER_UPLOAD_URL: '[https://your-worker.yourdomain.workers.dev](https://your-worker.yourdomain.workers.dev)', 
+    DEFAULT_AD_URL: '[https://example-ad-network.com/promote](https://example-ad-network.com/promote)'
+};
