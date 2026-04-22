8fdd8834978c61311dc0a737c1481d5644284506
ytruenightstory-png <ytruenightstory@gmail.com>
2026-04-22 08:36:41 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "[https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js](https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js)";
+import { getFirestore } from "[https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js](https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js)";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCDBVvf6Q18-cdcLouUVenDOeYPtfKOZt8",
+    authDomain: "stealth-drive-ca20e.firebaseapp.com",
+    projectId: "stealth-drive-ca20e",
+    storageBucket: "stealth-drive-ca20e.firebasestorage.app",
+    messagingSenderId: "72158519364",
+    appId: "1:72158519364:web:f17473d7446225b9a99c11"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
